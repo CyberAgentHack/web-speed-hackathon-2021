c3d0732d2408df41d467be2c03532ae2d807ecd1
kaaazu <wato9902@gmail.com>
2021-02-06 19:55:30 +0900

    convert webm

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames';
-import gifler from 'gifler';
+// import gifler from 'gifler';
 import React from 'react';
 
-import { fetchBinary } from '../../../utils/fetchers';
+// import { fetchBinary } from '../../../utils/fetchers';
 import { FontAwesomeIcon } from '../FontAwesomeIcon/FontAwesomeIcon';
 
 /**
@@ -15,39 +15,51 @@ import { FontAwesomeIcon } from '../FontAwesomeIcon/FontAwesomeIcon';
  * @type {React.VFC<Props>}
  */
 const PausableMovie = ({ src }) => {
-  /** @type {React.RefObject<HTMLImageElement>} */
-  const canvasRef = React.useRef(null);
-  /** @type {React.RefObject<import('gifler').Animator>} */
-  const animatorRef = React.useRef(null);
+  // /** @type {React.RefObject<HTMLImageElement>} */
+  // const canvasRef = React.useRef(null);
+  // /** @type {React.RefObject<import('gifler').Animator>} */
+  // const animatorRef = React.useRef(null);
+  /** @type {React.RefObject<HTMLVideoElement>} */
+  const videoRef = React.useRef(null);
 
-  React.useEffect(() => {
-    (async () => {
-      const data = await fetchBinary({ url: src });
-      // gifler.Gif は GIF を解析する
-      const gif = new gifler.Gif(Promise.resolve(data));
-      // gif.animate でアニメーションを制御 Animator が作られる
-      const animator = await gif.animate(canvasRef.current);
-      animatorRef.current = animator;
-    })();
+  // React.useEffect(() => {
+  //   (async () => {
+  // const data = await fetchBinary({ url: src });
+  // gifler.Gif は GIF を解析する
+  // const gif = new gifler.Gif(Promise.resolve(data));
+  // gif.animate でアニメーションを制御 Animator が作られる
+  // const animator = await gif.animate(canvasRef.current);
+  //     animatorRef.current = animator;
+  //   })();
 
-    return () => animatorRef.current?.stop();
-  }, []);
+  //   videoRef.current = animator;
+
+  //   return () => animatorRef.current?.stop();
+  // }, []);
 
   const [isPlaying, setIsPlaying] = React.useState(true);
 
   const handleClick = React.useCallback(() => {
     if (isPlaying) {
-      animatorRef.current?.stop();
+      videoRef.current?.pause();
       setIsPlaying(false);
     } else {
-      animatorRef.current?.start();
+      videoRef.current?.play();
       setIsPlaying(true);
     }
   }, [isPlaying]);
 
   return (
     <button className="group relative block w-full h-full" type="button" onClick={handleClick}>
-      <canvas ref={canvasRef} className="w-full" />
+      {/* <canvas ref={canvasRef} className="w-full" /> */}
+      {/* {src.includes('.gif') ? (
+        <img src={src} />
+      ) : ( */}
+      <video ref={videoRef} autoplay="true" loop="true" muted="true" playsinline="true">
+        <source src={src} type="video/webm" />
+      </video>
+      {/* )} */}
+
       <div
         className={classNames(
           'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
--- client/src/components/post/MovieArea/MovieArea.jsx
+++ client/src/components/post/MovieArea/MovieArea.jsx
@@ -14,7 +14,11 @@ const MovieArea = ({ movie }) => {
   return (
     <AspectRatioBox aspectHeight={1} aspectWidth={1}>
       <div className="relative w-full h-full bg-gray-300 border border-gray-300 rounded-lg overflow-hidden">
+        {/* <video autoplay="true" loop="true" muted="true" playsinline="true"> */}
+        {/* <source src={getMoviePath(movie.id)} type="video/webm" /> */}
         <PausableMovie src={getMoviePath(movie.id)} />
+        {/* </source> */}
+        {/* </video> */}
       </div>
     </AspectRatioBox>
   );
