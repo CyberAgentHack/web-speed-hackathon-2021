2e93ed700e965929a74e9cf5bf83bd36d5a2ec01
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2021-02-07 12:51:25 +0900

    helmetの削除

--- client/src/containers/TermContainer/TermContainer.jsx
+++ client/src/containers/TermContainer/TermContainer.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-
 import { TermPage } from '../../components/term/TermPage';
 
 /** @type {React.VFC} */
 const TermContainer = () => {
+  document.title = "利用規約- CAwitter";
   return (
     <>
-      <Helmet>
-        <title>利用規約- CAwitter</title>
-      </Helmet>
       <TermPage />
     </>
   );
