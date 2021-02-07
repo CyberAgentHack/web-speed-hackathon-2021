1ef4597be556d8560af54e9bd5943ec6b822ab25
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2021-02-07 13:43:57 +0900

    helmetの削除

--- client/src/containers/NotFoundContainer/NotFoundContainer.jsx
+++ client/src/containers/NotFoundContainer/NotFoundContainer.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import { Helmet } from 'react-helmet';
-
 import { NotFoundPage } from '../../components/application/NotFoundPage';
 
 /** @type {React.VFC} */
 const NotFoundContainer = () => {
+  document.title = "ページが見つかりません- CAwitter"
   return (
     <>
-      <Helmet>
-        <title>ページが見つかりません- CAwitter</title>
-      </Helmet>
       <NotFoundPage />
     </>
   );
