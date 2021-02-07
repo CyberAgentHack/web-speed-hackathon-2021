178c39dd0b2d3e5c9e07ba09d7306bc6455f24e1
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2021-02-07 12:48:24 +0900

    setting

--- client/babel.config.js
+++ client/babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
       '@babel/preset-env',
       {
         corejs: '3',
-        useBuiltIns: true,
+        useBuiltIns: "usage",
         modules: 'commonjs',
       },
     ],
