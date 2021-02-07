71f3ee74557151dc154e5b15e43a960a9301eb24
GotoRen <r0719en@pluslab.org>
2021-02-07 15:45:08 +0900

    🔥 core-jsとregenerator-runtimeの削除

--- client/babel.config.js
+++ client/babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
       '@babel/preset-env',
       {
         corejs: '3',
-        useBuiltIns: false,
+        useBuiltIns: "usage",
         modules: 'commonjs',
       },
     ],
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,8 +13,6 @@ const DIST_PATH = path.resolve(__dirname, '../dist');
 const config = {
   mode: 'production',
   entry: [
-    // 'core-js',
-    'regenerator-runtime/runtime',
     'jquery-binarytransport',
     path.resolve(SRC_PATH, './index.css'),
     path.resolve(SRC_PATH, './index.jsx'),
