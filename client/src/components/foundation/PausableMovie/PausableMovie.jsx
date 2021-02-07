6117637e51b97dcd4675adfc2470c9f9f141ca63
GotoRen <r0719en@pluslab.org>
2021-02-07 15:58:01 +0900

    🎨 動画の最適化（.gif -> .mp4）

--- client/src/components/foundation/PausableMovie/PausableMovie.jsx
+++ client/src/components/foundation/PausableMovie/PausableMovie.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import gifler from 'gifler';
+// import gifler from 'gifler';
 import React from 'react';
 
 import { fetchBinary } from '../../../utils/fetchers';
@@ -16,38 +16,45 @@ import { FontAwesomeIcon } from '../FontAwesomeIcon/FontAwesomeIcon';
  */
 const PausableMovie = ({ src }) => {
   /** @type {React.RefObject<HTMLImageElement>} */
-  const canvasRef = React.useRef(null);
+  // const canvasRef = React.useRef(null);
   /** @type {React.RefObject<import('gifler').Animator>} */
   const animatorRef = React.useRef(null);
 
-  React.useEffect(() => {
-    (async () => {
-      const data = await fetchBinary({ url: src });
-      // gifler.Gif は GIF を解析する
-      const gif = new gifler.Gif(Promise.resolve(data));
-      // gif.animate でアニメーションを制御 Animator が作られる
-      const animator = await gif.animate(canvasRef.current);
-      animatorRef.current = animator;
-    })();
+  const videoRef = React.useRef(null);
 
-    return () => animatorRef.current?.stop();
-  }, []);
+  // React.useEffect(() => {
+  //   (async () => {
+  //     const data = await fetchBinary({ url: src });
+  //     // gifler.Gif は GIF を解析する
+  //     const gif = new gifler.Gif(Promise.resolve(data));
+  //     // gif.animate でアニメーションを制御 Animator が作られる
+  //     const animator = await gif.animate(canvasRef.current);
+  //     animatorRef.current = animator;
+  //   })();
+
+  //   return () => animatorRef.current?.stop();
+  // }, []);
 
   const [isPlaying, setIsPlaying] = React.useState(true);
 
   const handleClick = React.useCallback(() => {
     if (isPlaying) {
-      animatorRef.current?.stop();
+      // animatorRef.current?.stop();
+      videoRef.current?.pause();
       setIsPlaying(false);
     } else {
-      animatorRef.current?.start();
+      // animatorRef.current?.start();
+      videoRef.current?.play();
       setIsPlaying(true);
     }
   }, [isPlaying]);
 
   return (
     <button className="group relative block w-full h-full" type="button" onClick={handleClick}>
-      <canvas ref={canvasRef} className="w-full" />
+      {/* <canvas ref={canvasRef} className="w-full" /> */}
+      <video ref={videoRef} loop muted autoplay="autoplay">
+        <source src={`.${src}`} type="video/mp4" />
+      </video>
       <div
         className={classNames(
           'absolute left-1/2 top-1/2 flex items-center justify-center w-16 h-16 text-white text-3xl bg-black bg-opacity-50 rounded-full transform -translate-x-1/2 -translate-y-1/2',
